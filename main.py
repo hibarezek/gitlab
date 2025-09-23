98bf97531605731203d2243dd94a060e2222e699
Hiba Rezek <hibam.2003rezek@gmail.com>
2025-09-23 14:30:05 +0300

    modifications for various files after rebase and squash

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 #this is the main file :)
+print("This is main.py")
 
 print("This is main.py againnnn")
 print("This is main.py")
+print("This is main.py")
+
+print("This is a modification in main.py") #q4 ex11
--- src/file1.py
+++ src/file1.py
@@ -2,3 +2,5 @@
 print("Hello World!")
 print("This is file 1") #ex2 q 9
 print("were are on branch master")
+
+#adding a new line for ex 3 q 11
