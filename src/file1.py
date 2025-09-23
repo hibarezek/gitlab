ab7223ef4afd93d70a7e4747a5659c4b41b84b9c
Hiba Rezek <hibam.2003rezek@gmail.com>
2025-09-23 09:20:25 +0300

    added additional information to data.txt and updated print statements in file1.py and newnamef2.py to reflect branch context

--- src/file1.py
+++ src/file1.py
@@ -1,3 +1,4 @@
 #modifying the contents of file1.py for ex 1 q 10
 print("Hello World!")
 print("This is file 1") #ex2 q 9
+print("were are on branch master")
--- src/newnamef2.py
+++ src/newnamef2.py
@@ -1,3 +1,4 @@
 print("hello world")
 print("This is file 2")
 print("This is a modification in newnamef2.py") #q2 ex10
+print("we are on master branch")
